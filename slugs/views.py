3648fc7e9f168e000f9a215ded7c59965e732d0b
chauh_harsh <chauhanharsh226@gmail.com>
2026-04-02 22:55:51 +0530

    Initialized the api setup

--- slugs/views.py
+++ slugs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['Get'])
+def home(request):
+    return Response("Welcome, This is the home page of Slugterra API.")
