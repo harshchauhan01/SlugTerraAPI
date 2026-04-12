27208d7247984cdcb4f2ea67d1c8ba8d85d6d720
chauh_harsh <chauhanharsh226@gmail.com>
2026-04-12 22:49:04 +0530

    Initiated caching via redis

--- config/settings.py
+++ config/settings.py
@@ -96,6 +96,18 @@ DATABASES = {
     }
 }
 
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/1")
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
